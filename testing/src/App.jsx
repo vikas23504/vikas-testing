e8c70f3aea21bd958796063f708ec237103b3c57
vikas23504 <vikas230704@gmail.com>
2026-05-28 12:03:36 +0800

    button created

--- testing/src/App.jsx
+++ testing/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
 const App = () => {
+  const handleclick = ()=>{
+    alert("hy i am alert ")
+  }
   return (
     <div>
-      <h4>i am testing got hub command</h4>
+      <h4>i am testing github command</h4>
+      <button onClick={handleclick}>click me </button>
     </div>
   )
 }
